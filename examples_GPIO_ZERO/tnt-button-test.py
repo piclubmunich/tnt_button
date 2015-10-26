a1d2ca1c3d8e88889ab19045a26f017ec95ed15c
Andreas Kopp <andreasrkopp@gmail.com>
2015-10-27 00:03:41 +0100

    Update tnt-button-test.py

--- examples_GPIO_ZERO/tnt-button-test.py
+++ examples_GPIO_ZERO/tnt-button-test.py
@@ -1,1 +1,21 @@
 #!/usr/bin/env python
+from gpiozero import LED
+from gpiozero import Button
+from time import sleep
+
+
+import mcpi.minecraft as minecraft
+mc = minecraft.Minecraft.create()
+import mcpi.block as block
+
+
+button = Button(24)
+
+
+while True:
+    if button.is_active:
+        print('Button Pressed')
+       
+        sleep(0.5)
+    else: tnt
+        print('press button to launch tnt')
