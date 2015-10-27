5e468285e92bacd8f249abbbc86bb53723b7c55a
Andreas Kopp <andreasrkopp@gmail.com>
2015-10-27 13:26:47 +0100

    Update tnt-button-test.py

--- example-RPI-GPIO/tnt-button-test.py
+++ example-RPI-GPIO/tnt-button-test.py
@@ -11,7 +11,7 @@ GPIO.setup(24, GPIO.IN) #setzt GPIO24 als Input
 try: 
     while True:  # das ist eine Endlosschleife, die mit CTRL+C stoppen kannst   
         if GPIO.input(24):  #when GPIO24 == 1 als wenn der Button gedrückt wird
-            print('Button Gedrückt')  #schreibe Button Grdückt
+            print('Button Gedrückt')  #schreibe Button Gedrückt
         else: 
             print("Drücke Button") #wenn Button nicht gedrückt dann schreibe Drücke Button
         sleep(0.2)  # warte 0.1 Sekunde
