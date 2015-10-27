ed9e6963c96ed414d7a46382350e6db723dcc29a
Andreas Kopp <andreasrkopp@gmail.com>
2015-10-27 13:25:47 +0100

    Update tnt-button-test.py

--- example-RPI-GPIO/tnt-button-test.py
+++ example-RPI-GPIO/tnt-button-test.py
@@ -1,26 +1,22 @@
+#!/usr/bin/env python
+
+import RPi.GPIO as GPIO # 
+from time import sleep # damit du Pausen einbauen kannst in deinen Code
+
+GPIO.setmode(GPIO.BCM) #du kannst zwischen zwei Pin Nummerierungsmodes wählen, hier wählen wir BCM
+
+GPIO.setup(24, GPIO.IN) #setzt GPIO24 als Input
 
 
-import RPi.GPIO as GPIO                                    # 
-import time
+try: 
+    while True:  # das ist eine Endlosschleife, die mit CTRL+C stoppen kannst   
+        if GPIO.input(24):  #when GPIO24 == 1 als wenn der Button gedrückt wird
+            print('Button Gedrückt')  #schreibe Button Grdückt
+        else: 
+            print("Drücke Button") #wenn Button nicht gedrückt dann schreibe Drücke Button
+        sleep(0.2)  # warte 0.1 Sekunde
+except KeyboardInterrupt:
+        GPIO.cleanup()  #resete den Pin 24 wieder
 
-GPIO.setmode(GPIO.BCM)
-
-GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-
-
-
-while True:
-    input_state = GPIO.input(24)
-    if input_state == False:
-        print('Button Pressed')
-        pos = mc.player.getPos()
-        x = pos.x
-        y = pos.y
-        z = pos.z
-
-        block = 46
-
-       	   mc.setBlock(x, y, z, block,1)
-        time.sleep(0.2)
 
 
