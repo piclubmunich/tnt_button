0b235d9389c6b03e9543449a4238804024e342aa
Andreas Kopp <andreasrkopp@gmail.com>
2015-11-01 00:17:37 +0100

    Update tnt-button-sound.py

--- example-RPI-GPIO/tnt-button-sound.py
+++ example-RPI-GPIO/tnt-button-sound.py
@@ -1,1 +1,32 @@
 #!/usr/bin/env python
+import RPi.GPIO as GPIO
+import time
+import o
+
+import mcpi.minecraft as minecraft
+mc = minecraft.Minecraft.create()
+import mcpi.block as block
+
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+
+
+while True:
+    input_state = GPIO.input(24)
+    if input_state == False:
+        print('Button Pressed')
+        pos = mc.player.getPos()
+        os.system('mpg123 minecraft_short.mp3 &')
+        x = pos.x
+        y = pos.y
+        z = pos.z
+
+        block = 46
+
+        mc.setBlock(x, y, z, block,1)
+        time.sleep(0.2)
+
+
