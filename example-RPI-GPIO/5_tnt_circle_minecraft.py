f890b5473c02d218c197cc39767eea54227be95b
Andreas Kopp <andreasrkopp@gmail.com>
2015-11-06 19:27:06 +0100

    Update 5_tnt_circle_minecraft.py

--- example-RPI-GPIO/5_tnt_circle_minecraft.py
+++ example-RPI-GPIO/5_tnt_circle_minecraft.py
@@ -14,17 +14,18 @@ radius = 10 #bestimme den Radius deines TNT-Kreises
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-while True:
-    input_state = GPIO.input(24)
-    if input_state == False:
-        print "Button Pressed"
-        mc.postToChat("TNT")
-        pos = mc.player.getPos()
-        x = pos.x
-        y = pos.y
-        z = pos.z
-        block = 46
-        mc.setBlock(x, y, z, block,1)	
-        mcdrawing.drawCircle(x, y, z, radius, block, 1)
-        sleep(0.2)
+try:
+    while True:
+        input_state = GPIO.input(24)
+        if input_state == False:
+            print "Button Pressed"
+            mc.postToChat("TNT")
+            pos = mc.player.getPos()
+            x = pos.x
+            y = pos.y
+            z = pos.z
+            block = 46
+            mc.setBlock(x, y, z, block,1)	
+            mcdrawing.drawCircle(x, y, z, radius, block, 1)
+            sleep(0.2)
 #mehr Aufgaben folgen
