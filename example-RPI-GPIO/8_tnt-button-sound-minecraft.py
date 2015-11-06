ca7d50dbe973ca06242534054a8c7d14871b5270
Andreas Kopp <andreasrkopp@gmail.com>
2015-11-06 21:09:52 +0100

    Update 8_tnt-button-sound-minecraft.py

--- example-RPI-GPIO/8_tnt-button-sound-minecraft.py
+++ example-RPI-GPIO/8_tnt-button-sound-minecraft.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python
+
+
+#zu halloween lade dir einen schoenen Schreisound herunter es sollte ein mp3 sein
+#kopiere es in den gleichen Folder wie dein Code
+# mit Internet Verbindung tippe folgendens sudo apt-get install mpg123
+#du kannst es den song so abspielen mpg123 MySong.mp3
+
 import RPi.GPIO as GPIO
 import time
 import os
@@ -11,8 +18,6 @@ import mcpi.block as block
 GPIO.setmode(GPIO.BCM)
 
 GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-
-
 
 while True:
     input_state = GPIO.input(24)
