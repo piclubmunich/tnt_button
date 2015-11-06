a74ad8f14fad9b4c4f2b0d1e39c072692d5f61de
Andreas Kopp <andreasrkopp@gmail.com>
2015-11-06 19:06:45 +0100

    Update 5_tnt_circle_minecraft.py

--- example-RPI-GPIO/5_tnt_circle_minecraft.py
+++ example-RPI-GPIO/5_tnt_circle_minecraft.py
@@ -17,8 +17,8 @@ GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 while True:
     input_state = GPIO.input(24)
     if input_state == False:
-        print „Button Pressed“
-        mc.postToChat(„TNT“)
+        print "Button Pressed"
+        mc.postToChat("TNT")
         pos = mc.player.getPos()
         x = pos.x
         y = pos.y
@@ -27,4 +27,4 @@ while True:
         mc.setBlock(x, y, z, block,1)	
         mcdrawing.drawCircle(x, y, z, radius, block, 1)
         sleep(0.2)
-#mehr Aufgaben bald https://github.com/piclubmunich/tnt_button
+#mehr Aufgaben folgen
