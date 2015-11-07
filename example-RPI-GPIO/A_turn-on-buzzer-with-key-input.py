9a6c411708b4e48f7f91adbf6ba6935ab6370dcc
Andreas Kopp <andreasrkopp@gmail.com>
2015-11-08 00:06:29 +0100

    Update A_turn-on-buzzer-with-key-input.py

--- example-RPI-GPIO/A_turn-on-buzzer-with-key-input.py
+++ example-RPI-GPIO/A_turn-on-buzzer-with-key-input.py
@@ -1,1 +1,15 @@
 #arbeite an code
+
+from time import sleep
+import uinput
+device =  uinput.Device((uinput.KEY_W,uinput.KEY_S))
+
+
+
+while 1:
+        device.emit(uinput.KEY_W,1)
+        print "hallo"
+        sleep(0.5)
+        device.emit(uinput.KEY_W,0)
+        print "hallo"
+        time.sleep(0.5)
