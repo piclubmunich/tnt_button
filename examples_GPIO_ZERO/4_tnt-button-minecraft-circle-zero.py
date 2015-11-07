a8a7e70901d8431c368f27548d73cf210cd288f1
Andreas Kopp <andreasrkopp@gmail.com>
2015-11-07 23:42:50 +0100

    Update 4_tnt-button-minecraft-circle-zero.py

--- examples_GPIO_ZERO/4_tnt-button-minecraft-circle-zero.py
+++ examples_GPIO_ZERO/4_tnt-button-minecraft-circle-zero.py
@@ -11,19 +11,16 @@ import mcpi.block as block
 
 radius = 10 #bestimme den Radius deines TNT-Kreises
 
-try:
-    while True:
-        if button.is_active:
-            print "Button Pressed"
-            mc.postToChat("TNT")
-            pos = mc.player.getPos()
-            x = pos.x
-            y = pos.y
-            z = pos.z
-            block = 46
-            mc.setBlock(x, y, z, block,1)	
-            mcdrawing.drawCircle(x, y, z, radius, block, 1)
-            sleep(0.2)
-finall:
-    GPIO.cleanup()
+while True:
+    if button.is_active:
+        print "Button Pressed"
+        mc.postToChat("TNT")
+        pos = mc.player.getPos()
+        x = pos.x
+        y = pos.y
+        z = pos.z
+        block = 46
+        mc.setBlock(x, y, z, block,1)	
+        mcdrawing.drawCircle(x, y, z, radius, block, 1)
+        sleep(0.2)
 #mehr Aufgaben folgen
