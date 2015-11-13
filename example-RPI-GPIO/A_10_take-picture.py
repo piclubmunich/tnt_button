b3aeedeefbe163cc1bd7fa483a4fefcba1d67c3d
Andreas Kopp <andreasrkopp@gmail.com>
2015-11-13 19:47:17 +0100

    Update A_10_take-picture.py

--- example-RPI-GPIO/A_10_take-picture.py
+++ example-RPI-GPIO/A_10_take-picture.py
@@ -1,1 +1,17 @@
 #arbeite gerade am code
+import picamera
+from RPi import GPIO
+
+button = 17
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(button, GPIO.IN, GPIO.PUD_UP)
+
+with picamera.PiCamera() as camera:
+	camera.start_preview()
+	frame = 1
+	while True:
+		GPIO.wait_for_edge(button, GPIO.FALLING)
+		camera.capture('/frame%03d.jpg' % frame)
+		frame += 1
+		camera.stop_preview()
