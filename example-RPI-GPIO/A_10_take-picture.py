7cc55883ec4e656cdb385bb65b726ff2432558bd
Andreas Kopp <andreasrkopp@gmail.com>
2015-11-13 21:04:13 +0100

    Update A_10_take-picture.py

--- example-RPI-GPIO/A_10_take-picture.py
+++ example-RPI-GPIO/A_10_take-picture.py
@@ -4,6 +4,7 @@
 #aktiviere die camera, tipp sudo kano-camera 
 #reboote deinen pi
 #tippe in das terminal ein raspistill -o image1.jpg  damit testest du ob die Kamera funktioniert 
+#erstelle einen neuen Orner mit dem Namen Animation
 
 import picamera
 from RPi import GPIO
@@ -14,11 +15,13 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(button, GPIO.IN, GPIO.PUD_UP)
 
 with picamera.PiCamera() as camera:
+	camera.vflip = True
+    	camera.hflip = True
 	camera.start_preview()
 	frame = 1
 	while True:
 		GPIO.wait_for_edge(button, GPIO.FALLING)
-		camera.capture('/frame%03d.jpg' % frame)
+		camera.capture('home/user/animation/frame%03d.jpg' % frame)
 		frame += 1
 		camera.stop_preview()
 
