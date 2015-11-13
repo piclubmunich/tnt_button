7fc475949bf9caf95e7be76a96be16cc05b3514b
Andreas Kopp <andreasrkopp@gmail.com>
2015-11-13 21:36:19 +0100

    Update A_12_stop-motion.py

--- example-RPI-GPIO/A_12_stop-motion.py
+++ example-RPI-GPIO/A_12_stop-motion.py
@@ -25,7 +25,17 @@ with picamera.PiCamera() as camera:
 		frame += 1
 	camera.stop_preview()
 
+#Stiching it
+#avconv -r 10 -qscale 2 -i animation/%03d.jpg animation.mp4
+
 
 #als ALternative kannst du auch eine Stopmotion Software installieren
-# sudo apt-get install uv4l uv4l-raspicam uv4l-raspicam-extras
+#wget http://www.linux-projects.org/listing/uv4l_repo/lrkey.asc && sudo apt-key add ./lrkey.asc
+#sudo sh -c 'echo "deb http://www.linux-projects.org/listing/uv4l_repo/raspbian/ wheezy main" >> /etc/apt/sources.list'
+#sudo apt-get update
+#sudo apt-get install uv4l uv4l-raspicam uv4l-raspicam-extras
+
+#sudo apt-get install uv4l uv4l-raspicam uv4l-raspicam-extras
 #sudo apt-get install stopmotion
+
+#
