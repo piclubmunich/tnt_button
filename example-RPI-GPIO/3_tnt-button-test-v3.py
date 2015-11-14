471f5ede93f3f793b2e6c9ba61f35ab4afae747a
Andreas Kopp <andreasrkopp@gmail.com>
2015-11-14 21:29:02 +0100

    Update 3_tnt-button-test-v3.py

--- example-RPI-GPIO/3_tnt-button-test-v3.py
+++ example-RPI-GPIO/3_tnt-button-test-v3.py
@@ -16,12 +16,10 @@ GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_UP) #setzt GPIO24 als Input mit pu
 
 try: 
     while True:  # das ist eine Endlosschleife, die mit CTRL+C stoppen kannst   
-        button_state = GPIO.input(24) #setze GPIO24 gleich der Variable button_state
-        if button_sate == False:  #wenn Button nicht nicht gedrueckt wird 
-            print "Button gedrueckt"  #schreibe Button gedrueckt
-        else: 
-            print "Button nicht gedrueckt" #wenn Button nicht gedrueckt, wenn du willst kannst due jetzt auch das ganze Else wegloeschen
-        sleep(0.1)
+        mein_buttin = GPIO.input(24) #setze GPIO24 gleich der Variable mein_button
+        if mein_button == False:  #wenn Button nicht nicht gedrueckt wird 
+            print "servus"
+            sleep(0.1)
 
 finally:
         GPIO.cleanup()  #resete den GPIO24 wieder
