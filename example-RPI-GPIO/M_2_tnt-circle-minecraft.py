bb0bc7d00ce05bb5f12d4a9279bab6461ccf94cb
Andreas Kopp <andreasrkopp@gmail.com>
2015-11-14 21:57:31 +0100

    Update and rename 8_tnt_circle_minecraft.py to M_2_tnt-circle-minecraft.py

--- example-RPI-GPIO/M_2_tnt-circle-minecraft.py
+++ example-RPI-GPIO/M_2_tnt-circle-minecraft.py
@@ -28,6 +28,6 @@ try:
             mc.setBlock(x, y, z, block,1)	
             mcdrawing.drawCircle(x, y, z, radius, block, 1)
             sleep(0.2)
-finall:
+finally:
     GPIO.cleanup()
 #mehr Aufgaben folgen
