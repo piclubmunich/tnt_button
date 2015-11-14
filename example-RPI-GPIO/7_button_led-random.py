e3f5aa93cd623e99e0874c49f4a670c1408ce599
Andreas Kopp <andreasrkopp@gmail.com>
2015-11-14 21:20:46 +0100

    Create 7_button_led-random.py

---
+++ example-RPI-GPIO/7_button_led-random.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+import RPi.GPIO as GPIO
+
+from time import sleep
+import random
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(24, GPIO.IN)
+GPIO.setup(17, GPIO.OUT)
+
+while True:
+  if not GPIO.input(11):
+    flash = random.randint(1,6)
+    while not GPIO.input(24):
+      GPIO.output(17, True)
+    while flash > 0:
+      GPIO.output(17, False)
+      sleep(.5)
+      GPIO.output(17, True)
+      sleep(.5)
+      flash ­= 1
+  else:
+    GPIO.output(17,True)
